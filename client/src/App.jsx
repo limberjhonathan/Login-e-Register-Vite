6594814cd0e21bb75aaa257adb56529c8640e3a2
Limber Jhonathan <limberjhonathan@gmail.com>
2024-08-02 20:59:19 -0300

    Dashboard, Login e Register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
 
 function App() {
 
   return (
     <div>
-      App
+      <Dashboard/>
+      <Login/>
+      <Register/>
     </div>
   )
 }
