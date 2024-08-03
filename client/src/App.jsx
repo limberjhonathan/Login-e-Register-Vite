918210be19d25ee59f69af56dd122bca4bc32fa5
Limber Jhonathan <limberjhonathan@gmail.com>
2024-08-02 21:15:31 -0300

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,31 @@ import Dashboard from './Components/Dashboard/Dashboard'
 import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
 
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><Login/></div>
+  },
+  {
+    path: '/register',
+    element: <div><Register/></div>
+  },
+  {
+    path: '/dashboard',
+    element: <div><Dashboard/></div>
+  },
+])
+
 function App() {
 
   return (
     <div>
-      <Dashboard/>
-      <Login/>
-      <Register/>
+      <RouterProvider router={router}/>
     </div>
   )
 }
