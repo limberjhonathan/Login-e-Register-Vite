7a8ef91b9a0af6acaec59e75e1b2bbeae9ec2a68
Limber Jhonathan <limberjhonathan@gmail.com>
2024-08-02 23:08:02 -0300

    Login

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,8 +1,46 @@
 // import React from "react";
 import './Login.css';
+import { Link } from 'react-router-dom';
+
+import video from '../../LoginAssets/video.mp4'
+import logo from '../../LoginAssets/logo.png'
 
 export default function Login() {
     return (
-        <div>Login</div>
+        <div className='loginPage flex'>
+            <div className="container flex">
+                <div className="videoDiv">
+                    <video src={video} autoPlay muted loop></video>
+                    <div className="textDiv">
+                        <h2 className='title'>Create and Sell Extraordinary Products</h2>
+                        <p>Adopt the peace of nature!</p>
+                    </div>
+
+                    <div className="footerDiv flex">
+                        <span className="text">Don't have an account?</span>
+                        <Link to={'/register'}>
+                            <button className="btn">Sign Up</button>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="formDiv flex">
+                    <div className="headerDiv">
+                        <img src={logo} alt='Logo Image'/>
+                        <h3>Welcome Back!</h3>
+                    </div>
+
+                    <form action="" className='form grid'>
+                        <sapn>Login Status will go here</sapn>
+                        <div className="inputDiv">
+                            <label htmlFor='username'>Username</label>
+                            <div className="input flex">
+                                
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
