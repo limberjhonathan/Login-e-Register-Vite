9a8e574cbfa38e1179047154908a5635da471722
Limber Jhonathan <limberjhonathan@gmail.com>
2024-08-06 23:46:13 -0300

    Login e Registro

---
+++ server/index.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'plantdb',
+    port: 3306
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database.');
+});
+
+app.post("/register", (req, res) => {
+    const sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    const values = [req.body.UserName, req.body.Email, req.body.Password];
+    db.query(sql, values, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.json({ Error: "Error" }); 
+        } else {
+            return res.json({ Success: "User registered successfully" });
+        }
+    });
+});
+
+app.post("/login", (req, res)=>{
+    const sentloginUserName = req.body.LoginUserName
+    const sentloginPassword = req.body.LoginPassword
+
+    const sql = "SELECT * FROM users WHERE username = ? && password = ?";
+    const values = [sentloginUserName, sentloginPassword];
+
+    db.query(sql, values, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.json({ error: err}); 
+        }
+        if(results.length > 0){
+            res.send(results)
+        }else{
+            res.send({message: 'Creadentials Don`t match!' })
+        }
+    });
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
