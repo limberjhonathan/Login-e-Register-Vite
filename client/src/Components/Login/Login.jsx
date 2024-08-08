30033928fe45de98442073b5265bf2db9a3202eb
Limber Jhonathan <limberjhonathan@gmail.com>
2024-08-07 21:20:05 -0300

    Ajustes no Login

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import './Login.css';
 import '../../App.css';
 import { Link, useNavigate } from 'react-router-dom';
@@ -7,29 +7,42 @@ import { BsFillShieldLockFill } from 'react-icons/bs';
 import { AiOutlineSwapRight } from 'react-icons/ai';
 import Axios from "axios";
 
-import video from '../../LoginAssets/video.mp4'
-import logo from '../../LoginAssets/logo.png'
+import video from '../../LoginAssets/video.mp4';
+import logo from '../../LoginAssets/logo.png';
 
 export default function Login() {
-    const [loginUserName, setLoginUserName] = useState('')
-    const [loginPassword, setLoginPassword] = useState('')
-    const navigateTo = useNavigate()
+    const [loginUserName, setLoginUserName] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+    const navigateTo = useNavigate();
+
+    const [loginStatus, setLoginStatus] = useState('');
+    const [statusHolder, setStatusHolder] = useState('message');
 
     const loginUser = (e) => {
         e.preventDefault();
         Axios.post('http://localhost:3000/login', {
             LoginUserName: loginUserName,
             LoginPassword: loginPassword
-        }).then((response)=>{
-            console.log(response)
+        }).then((response) => {
+            console.log(response);
 
-            if(response.data.message){
-                navigateTo('/')
-            }else{
-                navigateTo('/dashboard')
+            if (response.data.message) {
+                setLoginStatus('Credentials Don’t Exist!');
+                navigateTo('/');
+            } else {
+                navigateTo('/dashboard');
             }
-        })
-    }
+        });
+    };
+
+    useEffect(() => {
+        if (loginStatus !== '') {
+            setStatusHolder('showMessage');
+            setTimeout(() => {
+                setStatusHolder('message');
+            }, 4000);
+        }
+    }, [loginStatus]);
 
     return (
         <div className='loginPage flex'>
@@ -42,7 +55,7 @@ export default function Login() {
                     </div>
 
                     <div className="footerDiv flex">
-                        <span className="text">Don t have an account?</span>
+                        <span className="text">Don’t have an account?</span>
                         <Link to={'/register'}>
                             <button className="btn">Sign Up</button>
                         </Link>
@@ -55,32 +68,34 @@ export default function Login() {
                         <h3>Welcome Back!</h3>
                     </div>
 
-                    <form action="" className='form grid'>
-                        {/* <span className='message'>Login Status will go here</span> */}
+                    <form action="" className='form grid' onSubmit={loginUser}>
+                        <span className={statusHolder}>{loginStatus}</span>
                         <div className="inputDiv">
                             <label htmlFor='username'>Username</label>
                             <div className="input flex">
                                 <FaUserShield className='icon'/>
-                                <input type='text' id='username' placeholder='Enter Username' onChange={(e)=>{
-                                    setLoginUserName(e.target.value)}}/>
+                                <input type='text' id='username' placeholder='Enter Username' onChange={(e) => {
+                                    setLoginUserName(e.target.value);
+                                }}/>
                             </div>
                         </div>
                         <div className="inputDiv">
                             <label htmlFor='password'>Password</label>
                             <div className="input flex">
-                                <BsFillShieldLockFill  className='icon'/>
-                                <input type='password' id='password' placeholder='Enter Password' onChange={(e)=>{
-                                    setLoginPassword(e.target.value)}}/>
+                                <BsFillShieldLockFill className='icon'/>
+                                <input type='password' id='password' placeholder='Enter Password' onChange={(e) => {
+                                    setLoginPassword(e.target.value);
+                                }}/>
                             </div>
                         </div>
 
-                        <button type='submit' className='btn flex' onClick={loginUser}>
+                        <button type='submit' className='btn flex'>
                             <span>Login </span>
-                            <AiOutlineSwapRight  className='icon'/>
+                            <AiOutlineSwapRight className='icon'/>
                         </button>
 
                         <span className="forgotPassword">
-                            Forgot you password? <a href=''>Click Here</a>
+                            Forgot your password? <a href=''>Click Here</a>
                         </span>
                     </form>
                 </div>
