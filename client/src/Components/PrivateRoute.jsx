1d8d28dfb4c238b55f8d87930ff7c23be2792266
Limber Jhonathan <limberjhonathan@gmail.com>
2024-08-09 20:35:07 -0300

    Verificação do token valido

--- client/src/Components/PrivateRoute.jsx
+++ client/src/Components/PrivateRoute.jsx
@@ -1,10 +1,22 @@
-// import React from 'react';
+import { jwtDecode } from "jwt-decode";
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ element: Element }) => {
     const token = localStorage.getItem('token');
 
-    return token ? <Element /> : <Navigate to="/" replace />;
+    let isAuthenticated = false;
+    if (token) {
+        try {
+            const decodedToken = jwtDecode(token);
+            const currentTime = Date.now() / 1000;
+            if (decodedToken.exp > currentTime) {
+                isAuthenticated = true;
+            }
+        } catch (error) {
+            console.error('Token decoding failed:', error);
+        }
+    }
+    return isAuthenticated ? <Element token={token}/> : <Navigate to="/" replace />;
 };
 
 export default PrivateRoute;
